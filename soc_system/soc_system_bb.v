c1191d2bdca9fca50ab03f791bdf667c62c285a4
rad1an <rad1an.pm@proton.me>
2023-12-23 15:41:41 +0200

    Add initial state from cisc

---
+++ soc_system/soc_system_bb.v
@@ -0,0 +1,76 @@
+
+module soc_system (
+	clk_clk,
+	hps_0_h2f_reset_reset_n,
+	memory_mem_a,
+	memory_mem_ba,
+	memory_mem_ck,
+	memory_mem_ck_n,
+	memory_mem_cke,
+	memory_mem_cs_n,
+	memory_mem_ras_n,
+	memory_mem_cas_n,
+	memory_mem_we_n,
+	memory_mem_reset_n,
+	memory_mem_dq,
+	memory_mem_dqs,
+	memory_mem_dqs_n,
+	memory_mem_odt,
+	memory_mem_dm,
+	memory_oct_rzqin,
+	reset_reset_n,
+	sdram_address,
+	sdram_byte_enable,
+	sdram_read,
+	sdram_write,
+	sdram_write_data,
+	sdram_acknowledge,
+	sdram_read_data,
+	uart_RXD,
+	uart_TXD,
+	uart_in_data,
+	uart_in_error,
+	uart_in_valid,
+	uart_in_ready,
+	uart_out_ready,
+	uart_out_data,
+	uart_out_error,
+	uart_out_valid);	
+
+	input		clk_clk;
+	output		hps_0_h2f_reset_reset_n;
+	output	[14:0]	memory_mem_a;
+	output	[2:0]	memory_mem_ba;
+	output		memory_mem_ck;
+	output		memory_mem_ck_n;
+	output		memory_mem_cke;
+	output		memory_mem_cs_n;
+	output		memory_mem_ras_n;
+	output		memory_mem_cas_n;
+	output		memory_mem_we_n;
+	output		memory_mem_reset_n;
+	inout	[31:0]	memory_mem_dq;
+	inout	[3:0]	memory_mem_dqs;
+	inout	[3:0]	memory_mem_dqs_n;
+	output		memory_mem_odt;
+	output	[3:0]	memory_mem_dm;
+	input		memory_oct_rzqin;
+	input		reset_reset_n;
+	input	[21:0]	sdram_address;
+	input	[3:0]	sdram_byte_enable;
+	input		sdram_read;
+	input		sdram_write;
+	input	[31:0]	sdram_write_data;
+	output		sdram_acknowledge;
+	output	[31:0]	sdram_read_data;
+	input		uart_RXD;
+	output		uart_TXD;
+	input	[7:0]	uart_in_data;
+	input		uart_in_error;
+	input		uart_in_valid;
+	output		uart_in_ready;
+	input		uart_out_ready;
+	output	[7:0]	uart_out_data;
+	output		uart_out_error;
+	output		uart_out_valid;
+endmodule
---
+++ soc_system/soc_system_inst.v
@@ -0,0 +1,39 @@
+	soc_system u0 (
+		.clk_clk                 (<connected-to-clk_clk>),                 //             clk.clk
+		.hps_0_h2f_reset_reset_n (<connected-to-hps_0_h2f_reset_reset_n>), // hps_0_h2f_reset.reset_n
+		.memory_mem_a            (<connected-to-memory_mem_a>),            //          memory.mem_a
+		.memory_mem_ba           (<connected-to-memory_mem_ba>),           //                .mem_ba
+		.memory_mem_ck           (<connected-to-memory_mem_ck>),           //                .mem_ck
+		.memory_mem_ck_n         (<connected-to-memory_mem_ck_n>),         //                .mem_ck_n
+		.memory_mem_cke          (<connected-to-memory_mem_cke>),          //                .mem_cke
+		.memory_mem_cs_n         (<connected-to-memory_mem_cs_n>),         //                .mem_cs_n
+		.memory_mem_ras_n        (<connected-to-memory_mem_ras_n>),        //                .mem_ras_n
+		.memory_mem_cas_n        (<connected-to-memory_mem_cas_n>),        //                .mem_cas_n
+		.memory_mem_we_n         (<connected-to-memory_mem_we_n>),         //                .mem_we_n
+		.memory_mem_reset_n      (<connected-to-memory_mem_reset_n>),      //                .mem_reset_n
+		.memory_mem_dq           (<connected-to-memory_mem_dq>),           //                .mem_dq
+		.memory_mem_dqs          (<connected-to-memory_mem_dqs>),          //                .mem_dqs
+		.memory_mem_dqs_n        (<connected-to-memory_mem_dqs_n>),        //                .mem_dqs_n
+		.memory_mem_odt          (<connected-to-memory_mem_odt>),          //                .mem_odt
+		.memory_mem_dm           (<connected-to-memory_mem_dm>),           //                .mem_dm
+		.memory_oct_rzqin        (<connected-to-memory_oct_rzqin>),        //                .oct_rzqin
+		.reset_reset_n           (<connected-to-reset_reset_n>),           //           reset.reset_n
+		.sdram_address           (<connected-to-sdram_address>),           //           sdram.address
+		.sdram_byte_enable       (<connected-to-sdram_byte_enable>),       //                .byte_enable
+		.sdram_read              (<connected-to-sdram_read>),              //                .read
+		.sdram_write             (<connected-to-sdram_write>),             //                .write
+		.sdram_write_data        (<connected-to-sdram_write_data>),        //                .write_data
+		.sdram_acknowledge       (<connected-to-sdram_acknowledge>),       //                .acknowledge
+		.sdram_read_data         (<connected-to-sdram_read_data>),         //                .read_data
+		.uart_RXD                (<connected-to-uart_RXD>),                //            uart.RXD
+		.uart_TXD                (<connected-to-uart_TXD>),                //                .TXD
+		.uart_in_data            (<connected-to-uart_in_data>),            //         uart_in.data
+		.uart_in_error           (<connected-to-uart_in_error>),           //                .error
+		.uart_in_valid           (<connected-to-uart_in_valid>),           //                .valid
+		.uart_in_ready           (<connected-to-uart_in_ready>),           //                .ready
+		.uart_out_ready          (<connected-to-uart_out_ready>),          //        uart_out.ready
+		.uart_out_data           (<connected-to-uart_out_data>),           //                .data
+		.uart_out_error          (<connected-to-uart_out_error>),          //                .error
+		.uart_out_valid          (<connected-to-uart_out_valid>)           //                .valid
+	);
+
